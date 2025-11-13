bd9575711f2c8d2011be5cc09756b7d4599158a1
saim-alam1 <allbehassansaim@gmail.com>
2025-11-13 21:08:00 +0600

    Added Footer & Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./Header";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
 
 export default function App() {
   return (
     <>
       <Header />
+      <HeroSection />
+      <Footer />
     </>
   );
 }
