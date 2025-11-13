c4a6e3b9a4fe2f345c59769b258d8a8badd52a67
saim-alam1 <allbehassansaim@gmail.com>
2025-11-13 21:20:21 +0600

    Added Task Board

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import HeroSection from "./HeroSection";
+import TaskBoard from "./TaskBoard";
 
 export default function App() {
   return (
-    <>
+    <div className="flex flex-col items-center">
       <Header />
       <HeroSection />
+      <TaskBoard />
       <Footer />
-    </>
+    </div>
   );
 }
