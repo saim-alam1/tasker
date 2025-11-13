7b4748f5a871975f9bf55a6143ecef330bff7b57
saim-alam1 <allbehassansaim@gmail.com>
2025-11-14 00:18:49 +0600

    Dynamatically Loaded TaskList Data

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -4,19 +4,27 @@ import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
-  const [tasks, setTasks] = useState();
+  const defaultTasks = {
+    id: crypto.randomUUID(),
+    title: "Learn React Native",
+    description:
+      "Connect an existing API to a third-party database using secure methods",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTasks]);
 
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
-        {/* <!-- Search Box --> */}
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
-        {/* <!-- Search Box Ends --> */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
